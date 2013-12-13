19bcd4e2059f7d68737439886c5995174d5a4b57
turnschuh3000 <niko.juro@gmail.com>
2013-12-13 22:26:49 +0100

    changed a mistake

--- ia.c
+++ ia.c
@@ -71,19 +71,4 @@ int main(int argc, char* argv[]) {
         system("espeak -p 99 \"Bittr?\"");
         sleep(getSleepTime(randomMode));
     }
-}#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-  puts("Hey, Niko! ");
-  system("espeak -v german \"Hey Niko!\"");
-  sleep(1);
-  while (1) {
-    puts("IA! ");
-    system("espeak -v german \"IA\"");
-    sleep(1);
-    puts("Bidde? ");
-    system("espeak -p 99 \"Bittr\"");
-    sleep(1);
-  }
 }
