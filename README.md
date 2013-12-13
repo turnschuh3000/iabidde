b9dee70758f9faad2fe8ed16bb0a2c9d3f09c1f6
turnschuh3000 <niko.juro@gmail.com>
2013-12-13 22:12:34 +0100

    exciting new Version!

--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 
 ## Description
 
-This Program does an infinite loop with espeak an the printwords "IA?" and "Bidde?"
+This Program does an infinite loop with espeak and the printwords "IA?" and "Bidde?"
 
 Use it for several situations, especially, if you are really really sad.
 
 Jawoll!
 
+## NEW !!
+
+Now with the incredible "-random" mode!
+The Program can now print in random times, and some specials, the first one is the 30% possibility, that "IA" comes twice, and a 2% chance for a special print. Have Fun! :D
+
 ## Dependencies
 
-Use it with a Linux OS and espeak
+Use it with an Linux OS and espeak
 
 ## licenced under the WTFPL
 
--- ia.c
+++ ia.c
@@ -1,4 +1,77 @@
+/*
+ * IA Copyleft Team Guenther Software Productions
+ * Licensed unter WTF Public License
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+// Sleep-Zeiten in Sekunden
+#define MIN_SLEEP 1
+#define MAX_SLEEP 6
+
+#define RAGE_CHANCE 2
+
+int getRandomInt(int min, int max) {
+    return (random() % (max - min)) + min;
+}
+
+int getSleepTime(int randomMode) {
+    if (!randomMode) {
+        return 1;
+    } else {
+        return getRandomInt(MIN_SLEEP, MAX_SLEEP);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // Argumente
+    int randomMode = 0;
+    int i;
+    for (i = 1; i < argc; i++) {
+        char* argument = argv[i];
+        if (strcasecmp(argument, "-random") == 0) { // Random-Mode!
+            randomMode = 1;
+            srandom(time(NULL));
+            puts("RANDOM-MODE ACTIVATED!!!!!!!!\n\n\n");
+        }
+    }
+
+    int j;
+    int iaPrinted = 0; // Wurde "IA" schonmal ausgegeben?
+    puts("Hey Niko!");
+    system("espeak -v german \"Hey Niko!\"");
+    sleep(getSleepTime(randomMode));
+    while (1) {
+        if (randomMode) {
+            j = random() % 3;
+            if (j == 0) {
+                puts("IA! ");
+                system("espeak -v german \"IA\"");
+                iaPrinted = 1;
+                sleep(getSleepTime(randomMode));
+            }
+            
+            // Aufreger mit 2% Wahrscheinlichkeit. IA muss einmal ausgegeben
+            // worden sein.
+            if ((iaPrinted) && (getRandomInt(0, 100) <= RAGE_CHANCE)) {
+                puts("Was ist IA? Verdammt sag es doch einfach >:-(");
+                system("espeak -v german \"Was ist IA? Verdammt sag es doch einfach! Grrrr\"");
+                sleep(getSleepTime(randomMode));
+            }
+            
+        }
+        puts("IA! ");
+        system("espeak -v german \"IA\"");
+        iaPrinted = 1;
+        sleep(getSleepTime(randomMode));
+        puts("Bidde? ");
+        system("espeak -p 99 \"Bittr?\"");
+        sleep(getSleepTime(randomMode));
+    }
+}#include <stdio.h>
 #include <stdlib.h>
 
 int main() {
